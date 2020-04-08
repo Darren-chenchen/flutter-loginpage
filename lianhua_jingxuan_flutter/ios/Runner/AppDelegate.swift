f447dfc52ec748f2fc7bedb4d28a2d1e007336ab
陈亮陈亮 <chenliang@hd123.com>
2020-04-08 15:58:05 +0800

    初始化

--- lianhua_jingxuan_flutter/ios/Runner/AppDelegate.swift
+++ lianhua_jingxuan_flutter/ios/Runner/AppDelegate.swift
@@ -1,10 +1,6 @@
 import UIKit
 import Flutter
-import UpdateManager
 
-let FlutterMethodChannel_Name = "lianhua_jingxuan_flutter/mutual"
-let FlutterEventChannel_Name = "lianhua_jingxuan_flutter/event"
-let BaiduAk = "Ljas7q9oLFKAAPupEVjb7Ga34CxQP8AH"
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -14,14 +10,6 @@ let BaiduAk = "Ljas7q9oLFKAAPupEVjb7Ga34CxQP8AH"
   ) -> Bool {
     
     GeneratedPluginRegistrant.register(with: self)
-        
-    BaiduMapViewPlugin.registerWith(registry:  self)
-    
-    #if DEBUG
-        UpdateManager.shared.setThemeColor(UIColor.init(red: 0, green: 101/255, blue: 255/255, alpha: 1))
-        UpdateManager.shared.checkVersionUpdate()
-    #endif
-    
         
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
